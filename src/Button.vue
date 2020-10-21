3b216c47f6390dfd8927bbdf66aefa3d924be1c0
htz-html <1006676722@qq.com>
2020-10-21 16:40:35 +0800

    添加icon和iconPosition属性

--- src/Button.vue
+++ src/Button.vue
@@ -1,17 +1,27 @@
 <template>
-  <button class="g-button">
-    <slot></slot>
+  <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
   </button>
 </template>
 <script>
-export default {};
+export default {
+  props: ["icon", "iconPosition"], //left,right
+};
 </script>
-
 <style lang="scss">
 .g-button {
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
+  vertical-align: middle;
   font: inherit;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
@@ -24,6 +34,25 @@ export default {};
   }
   &:focus {
     outline: none;
+  }
+  > .icon {
+    order: 1;
+    margin-right: 0.1em;
+    width: 1em;
+    height: 1em;
+  }
+  > .content {
+    order: 2;
+  }
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: 0.1em;
+    }
+    > .content {
+      order: 1;
+    }
   }
 }
 </style>
