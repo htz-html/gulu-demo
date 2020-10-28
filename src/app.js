129ef4d58c7ba2481f1e31301f375f52753ac7e4
htz-html <1006676722@qq.com>
2020-10-28 08:34:48 +0800

    重测自动测试

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import Button from './button.vue'
-import Icon from './Icon.vue'
-import buttonGroup from './buttonGroup.vue'
+import Icon from './icon.vue'
+import ButtonGroup from './buttonGroup.vue'
 
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
-Vue.component('g-button-group', buttonGroup)
+Vue.component('g-button-group', ButtonGroup)
 
 new Vue({
   el: "#app",
