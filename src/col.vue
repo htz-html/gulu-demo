bfbda2fb611108b2cb43e6305d6230c674bd57c7
htz-html <1006676722@qq.com>
2020-11-03 10:31:16 +0800

    添加了offset

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`, offset && `offset-${offset}`]">
       <slot></slot>
     </div>
 </template>
@@ -9,6 +9,9 @@ export default {
   name:"GuluCol",
   props:{
     span:{
+      type:[Number,String]
+    },
+    offset:{
       type:[Number,String]
     }
   }
@@ -25,6 +28,12 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24)*100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24)*100%;
     }
   }
 }
