1b9eeb45f39adedb6036fba2c82019e625c444a6
htz-html <1006676722@qq.com>
2020-11-05 17:20:20 +0800

    基本完成gird

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import ButtonGroup from './buttonGroup.vue'
 import Input from './input.vue'
 import Col from './col.vue'
 import Row from './row.vue'
+import Layout from './layout.vue'
+import Header from './header.vue'
+import Content from './content.vue'
+import Sider from './sider.vue'
+import Footer from './footer.vue'
 
 
 Vue.component('g-button', Button)
@@ -13,6 +18,11 @@ Vue.component('g-button-group', ButtonGroup)
 Vue.component('g-input', Input)
 Vue.component('g-col', Col)
 Vue.component('g-row', Row)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
 
 new Vue({
   el: "#app",
